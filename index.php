b318629939ff8acce7cb8b087f2e2aad809f6ba3
Adams <jadams@IHC.EU>
2019-01-15 07:37:15 +1000

    Submission of base.

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+      <title> IHC Robbins Self-Serve </title>
+</head>
+
+<body>
+
+<form action="welcome.php" method="post">
+First Name: <input type="text" name="fir_name">
+<br>
+Surname: <input type="text" name="surname">
+<br>
+Company: <input type="text" name="company">
+<br>
+
+<input type="submit" name="generate" value="Submit!">
+</form>
+</body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,62 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["fir_name"]; ?><?php echo $_POST["surname"]; ?> to IHC Robbins!
+
+<br>
+
+Company:<?php echo $_POST["company"]; ?>
+
+<br>
+
+<?php
+if(isset($_POST['generate']))
+{
+  $FirstName = $_POST['fir_name'];
+  $Surname = $_POST['surname'];
+  $Company = $_POST['company'];
+ $text = $FirstName . $Surname . " - " . $Company;
+ echo "<img alt='testing' src='barcode/barcode.php?codetype=Code39&size=40&text=".$text."&print=true'/>";
+}
+?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<center><?php
+$servername = "localhost";
+$username = "selfserve";
+$password = "Patmyd0g";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+ $FirstName = $_POST['fir_name'];
+ $Surname = $_POST['surname'];
+
+
+$sql = "INSERT INTO guests (fir_name, surname, company, Barcode)
+VALUES ('$FirstName' , '$Surname' , '$Company' , '$text')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Returning to the Home Page in 15 Seconds ";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+</body>
+<?php header( "refresh:15;url=index.php" ); ?></center>
+
+</html>
